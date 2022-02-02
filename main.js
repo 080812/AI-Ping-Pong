0dc721d8a72131001c17b44fc042575398efa9ac
080812 <74765678+080812@users.noreply.github.com>
2022-02-02 14:50:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ function setup(){
   canvas.center()
   canvas.parent('canvas')
   video=createCapture(VIDEO)
+  video.size(700,600)
   poseNet=ml5.poseNet(video,modelLoaded)
   poseNet.on('pose',gotPoses)
   
