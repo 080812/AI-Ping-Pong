bf5cc80b71ee2844ce077a6a48b880a0ff97b5fc
080812 <74765678+080812@users.noreply.github.com>
2022-02-04 16:09:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-RWX=0
-LWY=0
+RWX=0;
+RWY=0;
+score=0;
 /*created by prashant shukla */
 
 var paddle2 =10,paddle1=10;
@@ -28,6 +29,7 @@ function setup(){
   canvas.center()
   canvas.parent('canvas')
   video=createCapture(VIDEO)
+  video.hide()
   video.size(700,600)
   poseNet=ml5.poseNet(video,modelLoaded)
   poseNet.on('pose',gotPoses)
@@ -41,6 +43,11 @@ function modelLoaded(){
 function gotPoses(results){
   if(results.length>0){
     console.log(results)
+
+    RWX=results[0].pose.rightWrist.x;
+    RWY=results[0].pose.leftWrist.y;
+
+    score=results[0].pose.keypoints[10].score
   }
   
 }
@@ -48,8 +55,11 @@ function gotPoses(results){
 
 
 function draw(){
-image(video,0,0,700,600)
+
  background(0); 
+ image(video,0,0,700,600)
+
+
 
  fill("black");
  stroke("black");
@@ -59,6 +69,11 @@ image(video,0,0,700,600)
  stroke("black");
  rect(0,0,20,700);
  
+ if(score>0){
+   fill("aqua")
+   stroke("yellow")
+   circle(RWX,RWY,29)
+ }
    //funtion paddleInCanvas call 
    paddleInCanvas();
  
